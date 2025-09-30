522c7e13ab8fc9dde9299169944a1cbfc80b877b
ThetHtar-Vivian <vivianthet76340@gmail.com>
2025-09-30 23:35:20 +0630

    foundational class

---
+++ src/main/java/com/napier/population/City.java
@@ -0,0 +1,18 @@
+package com.napier.population;
+
+public class City {
+    /** Primary key (ID) */
+    private int id;
+
+    /** City name */
+    private String name;
+
+    /** Country code (FK -> country.code) */
+   private  String country_code;
+
+    /** District */
+  private String district;
+
+    /** Population */
+   private int population;
+}
---
+++ src/main/java/com/napier/population/CountryLanguage.java
@@ -0,0 +1,15 @@
+package com.napier.population;
+
+public class CountryLanguage {
+    /** Country code (FK -> country.code) */
+    private String country_code;
+
+    /** Language name */
+    private String language;
+
+    /** Is official? ('T' or 'F') */
+    private String is_official;
+
+    /** Percentage of speakers (decimal(4,1)) */
+    private double percentage;
+}
