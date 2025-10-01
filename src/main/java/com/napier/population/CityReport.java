7c738d335c24a5e3bde1398f58f09ad5ebee6f2b
Aung-Paing-Khant <40794414@live.napier.ac.uk>
2025-10-01 23:26:12 +0630

    Get Top 10 Cities By Continent Population

--- src/main/java/com/napier/population/CityReport.java
+++ src/main/java/com/napier/population/CityReport.java
@@ -17,4 +17,44 @@ public class CityReport {
         this.con = con;
     }
 
+    /**
+     *
+     */
+    public ArrayList<City> getTop10CitiesByContinentPopulation() {
+        ArrayList<City> cities = new ArrayList<>();
+        try {
+            Statement stmt = con.createStatement();
+            String sql =
+                    "SELECT CityName, CountryName, District, Region, Continent, Population " +
+                            "FROM ( " +
+                            "    SELECT c.Name AS CityName, " +
+                            "           co.Name AS CountryName, " +
+                            "           c.District, " +
+                            "           co.Region, " +
+                            "           co.Continent, " +
+                            "           c.Population, " +
+                            "           ROW_NUMBER() OVER (PARTITION BY co.Continent ORDER BY c.Population DESC) AS rn " +
+                            "    FROM city c " +
+                            "    JOIN country co ON c.CountryCode = co.Code " +
+                            ") sub " +
+                            "WHERE rn <= 10 " +
+                            "ORDER BY Continent, Population DESC;";
+
+            ResultSet rset = stmt.executeQuery(sql);
+
+            while (rset.next()) {
+                City city = new City();
+                city.setName(rset.getString("CityName"));
+                city.setCountry_name(rset.getString("CountryName"));
+                city.setDistrict(rset.getString("District"));
+                city.setRegion(rset.getString("Region"));
+                city.setContinent(rset.getString("Continent"));
+                city.setPopulation(rset.getInt("Population"));
+                cities.add(city);
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to get top 10 cities by continent: " + e.getMessage());
+        }
+        return cities;
+    }
 }
