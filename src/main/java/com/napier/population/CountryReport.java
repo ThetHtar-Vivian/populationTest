c1790f560c0f3422899201aaccb6d10833a74dd7
Kritthegod <142289388+Kritthegod@users.noreply.github.com>
2025-10-03 22:56:08 +0630

    Generating Top 10 populated countries in asia

--- src/main/java/com/napier/population/CountryReport.java
+++ src/main/java/com/napier/population/CountryReport.java
@@ -10,60 +10,60 @@ import java.util.ArrayList;
  */
 public class CountryReport {
 
-    /**
-     * Database connection object used to query the database
-     */
-    private Connection con;
+        /**
+         * Database connection object used to query the database
+         */
+        private Connection con;
 
-    /**
-     * Constructor initializes the CountryReport with an active database connection.
-     *
-     * @param con Active database connection (from DbConnection)
-     */
-    public CountryReport(Connection con) {
-        this.con = con;
-    }
-
-    /**
-     * Get the top 10 populated countries in a given continent.
-     * @param continent The continent name
-     * @return ArrayList of Country objects
-     */
-    public ArrayList<Country> getTop10CountriesByContinentPopulation(String continent) {
-        ArrayList<Country> countries = new ArrayList<>();
-        try {
-            PreparedStatement stmt = con.prepareStatement(
-                    "SELECT co.Code, co.Name AS CountryName, co.Capital, co.Region, co.Continent, co.Population, " +
-                            "c.District FROM country co " +
-                            "LEFT JOIN city c ON co.Capital = c.ID " +
-                            "WHERE co.Continent = ? " +
-                            "ORDER BY co.Population DESC " +
-                            "LIMIT 10;"
-            );
-            stmt.setString(1, continent);
-
-            ResultSet rset = stmt.executeQuery();
-
-            while (rset.next()) {
-                Country country = new Country();
-                country.setCode(rset.getString("Code"));
-                country.setName(rset.getString("CountryName"));
-                country.setCapital(rset.getInt("Capital"));
-                country.setDistrict(rset.getString("District"));
-                country.setRegion(rset.getString("Region"));
-                country.setContinent(rset.getString("Continent"));
-                country.setPopulation(rset.getInt("Population"));
-                countries.add(country);
-            }
-
-            rset.close();
-            stmt.close();
-        } catch (SQLException e) {
-            System.out.println("Failed to get top 10 countries by continent: " + e.getMessage());
+        /**
+         * Constructor initializes the CountryReport with an active database connection.
+         *
+         * @param con Active database connection (from DbConnection)
+         */
+        public CountryReport(Connection con) {
+            this.con = con;
         }
 
-        return countries;
-    }
+        /**
+         * Get the top 10 populated countries in a given continent.
+         * @param continent The continent name
+         * @return ArrayList of Country objects
+         */
+        public ArrayList<Country> getTop10CountriesByContinentPopulation(String continent) {
+            ArrayList<Country> countries = new ArrayList<>();
+            try {
+                PreparedStatement stmt = con.prepareStatement(
+                        "SELECT co.Code, co.Name AS CountryName, co.Capital, co.Region, co.Continent, co.Population, " +
+                                "c.District FROM country co " +
+                                "LEFT JOIN city c ON co.Capital = c.ID " +
+                                "WHERE co.Continent = ? " +
+                                "ORDER BY co.Population DESC " +
+                                "LIMIT 10;"
+                );
+                stmt.setString(1, continent);
+
+                ResultSet rset = stmt.executeQuery();
+
+                while (rset.next()) {
+                    Country country = new Country();
+                    country.setCode(rset.getString("Code"));
+                    country.setName(rset.getString("CountryName"));
+                    country.setCapital(rset.getInt("Capital"));
+                    country.setDistrict(rset.getString("District"));
+                    country.setRegion(rset.getString("Region"));
+                    country.setContinent(rset.getString("Continent"));
+                    country.setPopulation(rset.getInt("Population"));
+                    countries.add(country);
+                }
+
+                rset.close();
+                stmt.close();
+            } catch (SQLException e) {
+                System.out.println("Failed to get top 10 countries by continent: " + e.getMessage());
+            }
+
+            return countries;
+        }
 
 
 }
