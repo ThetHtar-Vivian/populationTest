61daac9253258bfb724680e9828ece8a809dd9ca
Kyaw-Zayar-Thein <40794411@live.napier.ac.uk>
2025-10-03 23:04:16 +0630

    replace codes

--- src/main/java/com/napier/population/CountryReport.java
+++ src/main/java/com/napier/population/CountryReport.java
@@ -27,35 +27,6 @@ public class CountryReport {
      * Get all countries in the world organized by population (largest to smallest).
      * @return ArrayList of countries
      */
-    public ArrayList<Country> getAllCountriesByPopulationDesc() {
-        ArrayList<Country> countries = new ArrayList<>();
-        try {
-            Statement stmt = con.createStatement();
-            String sql =
-                    "SELECT co.Code, co.Name AS CountryName, co.Capital, co.Region, co.Continent, co.Population, " +
-                            "       c.District " +
-                            "FROM country co " +
-                            "LEFT JOIN city c ON co.Capital = c.ID " +
-                            "ORDER BY co.Population DESC;";
-
-            ResultSet rset = stmt.executeQuery(sql);
-
-            while (rset.next()) {
-                Country country = new Country();
-                country.setCode(rset.getString("Code"));
-                country.setName(rset.getString("CountryName"));
-                country.setCapital(rset.getInt("Capital"));
-                country.setDistrict(rset.getString("District"));
-                country.setRegion(rset.getString("Region"));
-                country.setContinent(rset.getString("Continent"));
-                country.setPopulation(rset.getInt("Population"));
-                countries.add(country);
-            }
-        } catch (SQLException e) {
-            System.out.println("Failed to get countries by population: " + e.getMessage());
-        }
-        return countries;
-    }
 
 
 }
