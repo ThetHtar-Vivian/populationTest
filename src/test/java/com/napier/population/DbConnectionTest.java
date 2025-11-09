d0a67d42bd7d3f88a5482a3cf9a0fe8e2f06d9c2
AungPaingK <40794414@live.napier.ac.uk>
2025-11-09 10:58:50 +0630

    Update Unit Test Stack

--- src/test/java/com/napier/population/DbConnectionTest.java
+++ src/test/java/com/napier/population/DbConnectionTest.java
@@ -13,24 +13,41 @@ class DbConnectionTest {
     DbConnection db;
     Connection con;
 
-    @BeforeEach
-    void setUp() {
-        db = new DbConnection();// Establish connection to the database
-        con = db.connect("localhost:33060", 10000);
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
-    void connect() {
+    void testConnectSuccess() {
+        DbConnection db = new DbConnection();
+        Connection con = db.connect("localhost:33060", 10000);
         assertNotNull(con);
     }
 
     @Test
-    void disconnect() {
+    void testConnectFailure() {
+        DbConnection db = new DbConnection();
+        Connection con = db.connect("invalid:3306", 0);
+        assertNull(con); // after 10 failed attempts
+    }
+
+    @Test
+    void testInterruptedDuringSleep() {
+        DbConnection db = new DbConnection();
+        Thread.currentThread().interrupt(); // force interruption
+
+        Connection con = db.connect("invalid:3306", 1000);
+        // interruption does not kill test, but covers the catch block
+        assertNull(con);
+    }
+
+    @Test
+    void testDisconnectSuccess() {
+        DbConnection db = new DbConnection();
+        db.connect("localhost:33060", 10000);
         int check = db.disconnect();
         assertEquals(1, check);
+    }
+
+    @Test
+    void testDisconnectWhenNull() {
+        DbConnection db = new DbConnection();
+        assertEquals(0, db.disconnect());
     }
 }
